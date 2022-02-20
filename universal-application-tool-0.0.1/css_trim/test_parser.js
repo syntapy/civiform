0fc125e0979542d20386e8332252e99d42360e98
Sami Abdul-Wahid <syntapy@pm.me>
2022-02-19 21:11:17 -0800

    Remove my own parser / lexer / tokens code

--- universal-application-tool-0.0.1/css_trim/test_parser.js
+++
@@ -1,9 +0,0 @@
-const parser = include('./parser')
-
-function test_1() {
-  const code = ' public static final String RING_OFFSET_PINK_200 = "ring-offset-pink-200";'
-  const result = parser.parseStyles(code)
-  console.log(result)
-}
-
-module.exports = { test_1 }
