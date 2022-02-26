38c8f6e86c4ad7d33c17da2dee8262c756ae462d
Sami Abdul-Wahid <syntapy@pm.me>
2022-02-25 19:23:03 -0800

    Draft some basic logic 1st steps in visitor

--- universal-application-tool-0.0.1/css_trim/visitor.js
+++ universal-application-tool-0.0.1/css_trim/visitor.js
@@ -2,6 +2,18 @@ const parser = require('java-parser')
 const traverse = require('./traverse')
 var _ = require('lodash')
 
+// TODO The logic in these visitor methods are looking to be very confusing.
+// And it also seems like it would be easy for the case where a developer uses
+// some more advanced / complex stylistic approaches in their code that would evade
+// some of the syntactic structures captured in this visitor.
+//
+// So that, combined with the complexity of the code, is kinda making me concerned about
+// the future maintainability of this thing
+//
+// Maybe theres a way to capture the logic in a more readable way?
+// And add tons of comments describing how this works so that someone can
+// easily jump on and make a modification in case a new coding style breaks
+// the style capture
 class CallsFinder extends parser.BaseJavaCstVisitorWithDefaults {
   constructor() {
     super()
@@ -20,204 +32,6 @@ class CallsFinder extends parser.BaseJavaCstVisitorWithDefaults {
     this.styleList = []
     this.tagList = []
     this.validateVisitor()
-  }
-
-  // All directly imported tags (from e.g. import j2html.TagCreator.<tag>) 
-  // will get added to tailwind.css
-  /*packageOrTypeName(ctx) {
-    super.packageOrTypeName(ctx)
-    let tag = ""
-
-    try {
-      let identifiers = ctx.Identifier
-
-      if (identifiers.length !== 3) { throw "Wrong identifier length. The j2html import we want here has length 3" }
-      if (identifiers[0].image !== "j2html") { throw "Not a j2thml import" }
-      if (identifiers[1].image !== "TagCreator") { throw "Not the right j2hml tag" }
-      if (identifiers[2].image === "each" || identifiers[2].image === "text") { throw "Not an html tag" }
-
-      tag = identifiers[2].image
-
-    } catch (error) {}
-
-    if (typeof(tag) === 'string') {
-      if (tag.length > 0) {
-        if (this.tagRegex.test(tag) === true) {
-          this.tagList.push(tag)
-        }
-      }
-    }
-  }*/
-
-  // Need to chain together the visitor methods so that all nodes are visited
-  // See: https://chevrotain.io/docs/guide/concrete_syntax_tree.html#traversing
-  primary(ctx) {
-    if (_.has(ctx, 'primaryPrefix')) {
-      for (const node of ctx.primaryPrefix) {
-        this.visit(node)
-      }
-    }
-
-    if (_.has(ctx, 'primarySuffix')) {
-      for (const node of ctx.primarySuffix) {
-        this.visit(node)
-      }
-    }
-  }
-
-  primaryPrefix(ctx) {
-    if (_.has(ctx, 'fqnOrRefType')) {
-      for (const node of ctx.fqnOrRefType) {
-        this.visit(node)
-      }
-    }
-  }
-
-  unaryExpression(ctx) {
-    if (_.has(ctx, 'primary')) {
-      for (const node of ctx.primary) {
-        this.visit(node)
-      }
-    }
-  }
-
-  binaryExpression(ctx) {
-    if (_.has(ctx, 'unaryExpression')) {
-      for (const node of ctx.unaryExpression) {
-        this.visit(node)
-      }
-    }
-  }
-
-  ternaryExpression(ctx) {
-    if (_.has(ctx, 'binaryExpression')) {
-      for (const node of ctx.binaryExpression) {
-        this.visit(node)
-      }
-    }
-  }
-
-  expression(ctx) {
-    if (_.has(ctx, 'ternaryExpression')) {
-      for (const node of ctx.ternaryExpression) {
-        this.visit(node)
-      }
-    }
-  }
-
-  argumentList(ctx) {
-    if (_.has(ctx, 'expression')) {
-      for (const node of ctx.expression) {
-        this.visit(node)
-      }
-    }
-  }
-
-  methodInvocationSuffix(ctx) {
-    if (_.has(ctx, 'argumentList')) {
-      for (const node of ctx.argumentList) {
-        this.visit(node)
-      }
-    }
-  }
-
-  primarySuffix(ctx) {
-    if (_.has(ctx, 'methodInvocationSuffix')) {
-      for (const node of ctx.methodInvocationSuffix) {
-        this.visit(node)
-      }
-    }
-  }
-
-  fqnOrRefType(ctx) {
-    if (_.has(ctx, 'fqnOrRefTypePartFirst')) {
-      for (const node of ctx.fqnOrRefTypePartFirst) {
-        this.visit(node)
-      }
-    }
-
-    if (_.has(ctx, 'fqnOrRefTypePartRest')) {
-      for (const node of ctx.fqnOrRefTypePartRest) {
-        this.visit(node)
-      }
-    }
-  }
-
-  fqnOrRefTypePartFirst(ctx) {
-    if (_.has(ctx, 'fqnOrRefTypePartCommon')) {
-      for (const node of ctx.fqnOrRefTypePartCommon) {
-        this.visit(node)
-      }
-    }
-  }
-
-  fqnOrRefTypePartCommon(ctx) {
-    console.log(ctx)
-    if (_.has(ctx, 'Identifier')) {
-      for (const node of ctx.Identifier) {
-        // Access identifier
-      }
-    }
-  }
-
-  fqnOrRefTypePartRest(ctx) {
-    if (_.has(ctx, 'fqnOrRefTypePartCommon')) {
-      for (const node of ctx.fqnOrRefTypePartCommon) {
-        this.visit(node)
-      }
-    }
-  }
-}
-
-const visitor = new CallsFinder()
-
-module.exports = visitor
-
-
-/*class CallsFinder extends parser.BaseJavaCstVisitorWithDefaults {
-  constructor() {
-    super()
-
-    // Just make sure we're not overwriting anything from super class
-    if (_.has(this, 'prefixList')) { throw "CallsFinder already has property 'prefixList'. Use a different property name to hold dictionary of prefix calls." }
-    if (_.has(this, 'styleList')) { throw "CallsFinder already has property 'styleList'. Use a different property name to hold dictionary of styles." }
-    if (_.has(this, 'styleRegex')) { throw "CallsFinder already has property 'styleRegex'. Use a different property name to hold dictionary of styles." }
-    if (_.has(this, 'tagList')) { throw "CallsFinder already has property 'tagList'. Use a different property name to hold dictionary of styles." }
-    if (_.has(this, 'styleRegex')) { throw "CallsFinder already has property 'tagRegex'. Use a different property name to hold dictionary of styles." }
-    if (_.has(this, '_findCalls')) { throw "CallsFinder already has property '_findCalls'. Use a different property name to hold dictionary of styles." }
-    if (_.has(this, '_findTags')) { throw "CallsFinder already has property '_findTags'. Use a different property name to hold dictionary of styles." }
-
-    this.callRegex = /[0-9A-Z_]+/
-    this.tagRegex = /[0-6a-z:]+/
-    this.styleList = []
-    this.tagList = []
-    this.validateVisitor()
-  }
-
-  // All directly import tags (from e.g. import j2html.TagCreator.<tag>) 
-  // will get added to tailwind.css
-  packageOrTypeName(ctx) {
-    let tag = ""
-
-    try {
-      let identifiers = ctx.Identifier
-
-      if (identifiers.length !== 3) { throw "Wrong identifier length. The j2html import we want here has length 3" }
-      if (identifiers[0].image !== "j2html") { throw "Not a j2thml import" }
-      if (identifiers[1].image !== "TagCreator") { throw "Not the right j2hml tag" }
-      if (identifiers[2].image === "each" || identifiers[2].image === "text") { throw "Not an html tag" }
-
-      tag = identifiers[2].image
-
-    } catch (error) {}
-
-    if (typeof(tag) === 'string') {
-      if (tag.length > 0) {
-        if (this.tagRegex.test(tag) === true) {
-          this.tagList.push(tag)
-        }
-      }
-    }
   }
 
   // Find calls to Styles.XYZ or BaseStyles.XYZ
@@ -283,13 +97,225 @@ module.exports = visitor
     }
   }
 
-  // Method name 'fqnOrRefType' matches grammar rule pattern 
-  // in java-parser/src/production/expression.js
-  //
-  // Hence during CST visiting it runs on every instance of that rule found during parsing
-  fqnOrRefType(ctx) {
-    console.log(ctx)
-    this._findCalls(ctx)
-    this._findTags(ctx)
+  // All directly imported tags (from e.g. import j2html.TagCreator.<tag>) 
+  // will get added to tailwind.css
+  /*packageOrTypeName(ctx) {
+    super.packageOrTypeName(ctx)
+    let tag = ""
+
+    try {
+      let identifiers = ctx.Identifier
+
+      if (identifiers.length !== 3) { throw "Wrong identifier length. The j2html import we want here has length 3" }
+      if (identifiers[0].image !== "j2html") { throw "Not a j2thml import" }
+      if (identifiers[1].image !== "TagCreator") { throw "Not the right j2hml tag" }
+      if (identifiers[2].image === "each" || identifiers[2].image === "text") { throw "Not an html tag" }
+
+      tag = identifiers[2].image
+
+    } catch (error) {}
+
+    if (typeof(tag) === 'string') {
+      if (tag.length > 0) {
+        if (this.tagRegex.test(tag) === true) {
+          this.tagList.push(tag)
+        }
+      }
+    }
+  }*/
+
+  // This is where most of the decision making in regards whether it encountered
+  // e.g. a StyleUtils.uvwXY(..), a Styles.XYZ, or a BaseStyles.UVW
+  // Need to chain together the visitor methods so that all nodes are visited
+  // See: https://chevrotain.io/docs/guide/concrete_syntax_tree.html#traversing
+  primary(ctx) {
+    const primaryResult = {
+      prefix: [],
+      suffix: [],
+    }
+    if (_.has(ctx, 'primaryPrefix')) {
+      for (const node of ctx.primaryPrefix) {
+        const resultIterable = this.visit(node)
+        for (const result of resultIterable) {
+          if (result === 'Styles') {
+            primaryResult.prefix.push(result)
+          }
+        }
+      }
+    }
+
+    if (_.has(ctx, 'primarySuffix')) {
+      for (const node of ctx.primarySuffix) {
+        const resultIterable = this.visit(node)
+        //for (const result of resultIterable) {
+        //}
+      }
+    }
+
+    return primaryResult
   }
-}*/
+
+  primaryPrefix(ctx) {
+    if (_.has(ctx, 'fqnOrRefType')) {
+      for (const node of ctx.fqnOrRefType) {
+        const iterList = []
+        for (const iter of this.visit(node)) {
+          iterList.push(iter)
+        }
+        return iterList
+      }
+    }
+
+    return null
+  }
+
+  unaryExpression(ctx) {
+    if (_.has(ctx, 'primary')) {
+      for (const node of ctx.primary) {
+        this.visit(node)
+      }
+    }
+  }
+
+  binaryExpression(ctx) {
+    if (_.has(ctx, 'unaryExpression')) {
+      for (const node of ctx.unaryExpression) {
+        this.visit(node)
+      }
+    }
+  }
+
+  ternaryExpression(ctx) {
+    if (_.has(ctx, 'binaryExpression')) {
+      for (const node of ctx.binaryExpression) {
+        this.visit(node)
+      }
+    }
+  }
+
+  expression(ctx) {
+    if (_.has(ctx, 'ternaryExpression')) {
+      for (const node of ctx.ternaryExpression) {
+        this.visit(node)
+      }
+    }
+  }
+
+  argumentList(ctx) {
+    if (_.has(ctx, 'expression')) {
+      for (const node of ctx.expression) {
+        this.visit(node)
+      }
+    }
+  }
+
+  methodInvocationSuffix(ctx) {
+    if (_.has(ctx, 'argumentList')) {
+      for (const node of ctx.argumentList) {
+        this.visit(node)
+      }
+    }
+  }
+
+  primarySuffix(ctx) {
+    if (_.has(ctx, 'methodInvocationSuffix')) {
+      for (const node of ctx.methodInvocationSuffix) {
+        this.visit(node)
+      }
+    }
+  }
+
+  *fqnOrRefType(ctx) {
+    //this._findCalls(ctx)
+    //this._findTags(ctx)
+    if (_.has(ctx, 'fqnOrRefTypePartFirst')) {
+      const node = ctx.fqnOrRefTypePartFirst
+      const length = node.length
+
+      // Could loop through it, but since left side of the fork
+      // here is expected to be length 1 only, mmight as well
+      // constrain it here
+      if (length > 1) {
+        throw "Unexpected length of fqnOrRefTypePartFirst list"
+      } else if (length === 1) {
+        yield this.visit(node[0])
+      }
+
+      //for (const node of nodeIter) {
+      //  yield this.visit(node)
+      //}
+    }
+
+    // Could be a full expression under here
+    // Which would be needed to be processed if its arguments
+    // to a 'StyleUtils' method call
+    //
+    // Or such a thing may be higher up the tree
+    if (_.has(ctx, 'fqnOrRefTypePartRest')) {
+      for (const node of ctx.fqnOrRefTypePartRest) {
+        // This may or may not bring anything on each yield pass
+        // depending on whether an 'expression' is ultimately under it
+        yield this.visit(node)
+      }
+    }
+  }
+
+  fqnOrRefTypePartFirst(ctx) {
+    if (_.has(ctx, 'fqnOrRefTypePartCommon')) {
+      const node = ctx.fqnOrRefTypePartCommon
+      const length = node.length
+      if (length > 1) {
+        throw "Unexpected length of fqnOrRefTypePartCommon list"
+      } else if (length === 1) {
+        return this.visit(node[0])
+      }
+      //for (const node of ctx.fqnOrRefTypePartCommon) {
+      //  this.visit(node)
+      //}
+    }
+
+    return null
+  }
+
+  // TODO: May need to loop through the sub visits of
+  // this one cuz it may have multiple ones for chained calls
+  // For example: 
+  // div().with(
+  //      StylesUtils
+  //        .responsiveMedium(...)
+  //      .with(
+  //        SytyleUtils.responsiveLarge(...)
+  //      )
+  //    )
+  fqnOrRefTypePartRest(ctx) {
+    if (_.has(ctx, 'fqnOrRefTypePartCommon')) {
+      const nodeIter = ctx.fqnOrRefTypePartCommon
+      const length = nodeIter.length
+      for (const node of ctx.fqnOrRefTypePartCommon) {
+        this.visit(node)
+      }
+    }
+  }
+
+  // Forms similar to the base case in recursive
+  fqnOrRefTypePartCommon(ctx) {
+    if (_.has(ctx, 'Identifier')) {
+      const node = ctx.Identifier
+      const length = node.length
+      if (length > 1) {
+        throw "Unexpected length of identifier list"
+      } else if (length === 1) {
+        const image = node[0].image
+        return image
+      }
+      //for (const node of ctx.Identifier) {
+      //}
+    }
+
+    return null
+  }
+}
+
+const visitor = new CallsFinder()
+
+module.exports = visitor
