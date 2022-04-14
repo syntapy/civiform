731151b1fbcafbec26ecb08509bbfedaa45b5ccc
Sami Abdul-Wahid <syntapy@pm.me>
2022-04-13 17:45:55 -0700

    TranslationFormView, LinkElement conversion

--- universal-application-tool-0.0.1/app/views/components/LinkElement.java
+++ universal-application-tool-0.0.1/app/views/components/LinkElement.java
@@ -10,6 +10,8 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import j2html.TagCreator;
+import j2html.tags.specialized.DivTag;
+import j2html.tags.specialized.FormTag;
 
 import play.filters.csrf.CSRF;
 import play.mvc.Http;
@@ -77,24 +79,24 @@ public class LinkElement {
     return this;
   }
 
-  public ContainerTag asAnchorText() {
-    ContainerTag tag = Strings.isNullOrEmpty(href) ? div(text) : a(text).withHref(href);
+  public DivTag asAnchorText() {
+    DivTag tag = Strings.isNullOrEmpty(href) ? div(text) : div().with(a(text).withHref(href));
     return tag.withCondId(!Strings.isNullOrEmpty(id), id)
         .withCondHref(!Strings.isNullOrEmpty(href), href)
         .withClasses(DEFAULT_LINK_STYLES, styles);
   }
 
-  public ContainerTag asButton() {
-    ContainerTag tag = Strings.isNullOrEmpty(href) ? div(text) : a(text).withHref(href);
+  public DivTag asButton() {
+    DivTag tag = Strings.isNullOrEmpty(href) ? div(text) : div().with(a(text).withHref(href));
     return tag.withCondId(!Strings.isNullOrEmpty(id), id)
         .withClasses(DEFAULT_LINK_BUTTON_STYLES, styles);
   }
 
-  public ContainerTag asHiddenForm(Http.Request request) {
+  public FormTag asHiddenForm(Http.Request request) {
     return this.asHiddenForm(request, ImmutableMap.of());
   }
 
-  public ContainerTag asHiddenForm(
+  public FormTag asHiddenForm(
       Http.Request request, ImmutableMap<String, String> hiddenFormValues) {
     Preconditions.checkNotNull(href);
     Option<CSRF.Token> csrfTokenMaybe = CSRF.getToken(request.asScala());
@@ -103,7 +105,7 @@ public class LinkElement {
       csrfToken = csrfTokenMaybe.get().value();
     }
 
-    ContainerTag form =
+    FormTag form =
         form(
                 input().isHidden().attr("value", csrfToken).attr("name", "csrfToken"),
                 button(TagCreator.text(text))
@@ -118,7 +120,7 @@ public class LinkElement {
     return form;
   }
 
-  public ContainerTag asHiddenFormLink(Http.Request request) {
+  public FormTag asHiddenFormLink(Http.Request request) {
     Preconditions.checkNotNull(href);
     Option<CSRF.Token> csrfTokenMaybe = CSRF.getToken(request.asScala());
     String csrfToken = "";
@@ -126,7 +128,7 @@ public class LinkElement {
       csrfToken = csrfTokenMaybe.get().value();
     }
 
-    ContainerTag form =
+    FormTag form =
         form(
                 input().isHidden().attr("value", csrfToken).attr("name", "csrfToken"),
                 button(TagCreator.text(text))
